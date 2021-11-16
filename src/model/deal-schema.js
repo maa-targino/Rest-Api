515de1a856b519fb9882c5dbd1c6db3b9c2b93c8
Marco Targino <maa.targino@gmail.com>
2021-11-16 03:42:04 -0300

    fix mistyping

--- src/model/deal-schema.js
+++ src/model/deal-schema.js
@@ -5,7 +5,7 @@ const dealSchema = mongoose.Schema({
     status:{type: String, required: true},
     value: {type: Number, required: true},
     quantity:{type: Number, required: true},
-    amount: {type: Number, default: function(){ return this.price * this.quantity }},
+    amount: {type: Number, default: function(){ return this.value * this.quantity }},
     currency: {type: String, required: true},
     add_time: {type: Date, default: Date.now}
 })
